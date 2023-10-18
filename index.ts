7c94ca8b1bc12b9a8cb2a89df2b750e4825ad0c6
Igor Koneshov <igorkoneshov@mail.ru>
2023-10-19 00:22:50 +0300

    test: types

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import { Store } from './lib/store'
 
 export const createStore = <T>(defaultValue: T): Store<T> => {
-    const store = new Store(defaultValue);
-    return store;
+    return new Store(defaultValue);
 }
